e389169b4482ea50ab321f159047dc435530d7b0
chengpeiquan <chengpeiquan@tinytiger.cn>
2020-09-28 17:35:10 +0800

    移除掉多余的定义

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,21 +1,8 @@
-import PushCNZZ from '@m/pushCNZZ'
+export default {};
 
 declare global {
-  interface Window {
-    _czc: any
-  }
-
   interface FileInfo {
     base64: any
     blob: any
-  }
-  
-  interface Vue {
-    prototype: any
-    $pushCNZZ: PushCNZZ
-  }
-  
-  interface To {
-    fullPath: string
   }
 }
